14b9d4bd7c82024bb44b1743a5cd6996e5e680d2
Nikita Turda <turda.nikita.bsn@gmail.com>
2024-02-14 12:23:00 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,9 @@
+INSTRUCTION:
+
+1) In IDE open the folder where you want to download the project
+
+After that in terminal
+
+2) git clone https://github.com/nikiturka/autoria 
+2) cd autoria
+3) docker-compose up
--- main.py
+++ main.py
@@ -8,6 +8,12 @@ import logging
 load_dotenv("/app/.env")
 load_dotenv("/app/.env.example")
 
+db_name = os.environ.get("DOCKER_POSTGRES_DB")
+db_user = os.environ.get("DOCKER_POSTGRES_USER")
+db_password = os.environ.get("DOCKER_POSTGRES_PASSWORD")
+db_host = os.environ.get("DOCKER_POSTGRES_HOST")
+db_port = os.environ.get("DOCKER_POSTGRES_PORT")
+
 
 def run_spider():
     logging.info("Running spider...")
@@ -18,14 +24,7 @@ def create_db_dump():
     logging.info("Creating database dump...")
 
     current_timestamp = time.strftime("%Y%m%d%H%M%S")
-    dump_filename = f"dumps/db_dump_{current_timestamp}.sql.gz"
-
-    db_name = os.environ.get("DOCKER_POSTGRES_DB")
-    db_user = os.environ.get("DOCKER_POSTGRES_USER")
-    db_password = os.environ.get("DOCKER_POSTGRES_PASSWORD")
-    db_host = os.environ.get("DOCKER_POSTGRES_HOST")
-    db_port = os.environ.get("DOCKER_POSTGRES_PORT")
-
+    dump_filename = f"dumps/db_dump_{current_timestamp}.sql"
     db_url = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
     with open(dump_filename, 'wb') as f:
